e0721c3ce85344298918726d92b49ae30048c67a
Jeremy Tay <jeremytay@Jeremys-MacBook-Pro.local>
2019-07-27 19:10:26 +1000

    twitter working

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -1,16 +1,45 @@
 import tweepy
 import json
+import os
+
+import .helper.utils
 
 # https://developer.twitter.com/en/docs.html
+# can use bounding box for location filtering 
+# - POSTED from the webpage? 
 
-with open("credentials.json", "r") as file:
-    creds = json.load(file)
+def sign_in():
+    """
+    returns a reference to the authenticated api
+    """
+    auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
+    auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
+    api = tweepy.API(auth)
+    return api
 
-auth = tweepy.OAuthHandler(creds["CONSUMER_KEY"], creds["CONSUMER_SECRET"])
-auth.set_access_token(creds["ACCESS_TOKEN"], creds["ACCESS_SECRET"])
+def scrape(location="", topic="", limit=""):
+    """
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+    """
+    # API.search(q[, geocode][, lang][, locale][, result_type][, count][, until][, since_id][, max_id][, include_entities])
+    tweets = []
+    if not location and not topic:
+        return tweets
+    api = sign_in()
+    query       = topic
+    geocode     = "-33.865143, 151.209900"
+    lang        = "EN"
+    locale      = ""
+    count       = 100
+    
+    search_results = api.search(topic,lang=lang,count=count)
+    for tweet in search_results:
+        tweets.append(tweet.text)
+    return tweets
 
-api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+tweets = scrape("","soccer")
+for tweet in tweets:
+    print(tweet)
