cc2282ee57cb67ace4ce2d42e736d75ac1357968
VinceMu <vince1997mu@gmail.com>
2019-07-29 21:48:47 +1000

    done

--- web/hack-app/src/components/Results/ResultsPage.js
+++ web/hack-app/src/components/Results/ResultsPage.js
@@ -7,29 +7,29 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom"
 
 
 var Chart = require("chart.js");
 const Print = (props) => (
     <h2>
-        {props.message} 
+        {props.message}
         {/* access message property of props */}
     </h2>
 )
 
 const useStyles = makeStyles(theme => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
     },
-  }));
+}));
 
- class ResultsPage extends Component {
+class ResultsPage extends Component {
 
 
     constructor(props) {
@@ -37,30 +37,31 @@ const useStyles = makeStyles(theme => ({
         super(props);
         this.state = {
             entityData: {
-                labels:[],
+                labels: [],
                 //labels: ['Germany', 'Spain', 'Mexico'],
                 datasets: [
                     {
                         // label: "Sentiment",
                         // data: ['0.2973507046699524','0.8444602489471436','0.9474703073501587']
-                        label:[],
-                        backgroundColor:[],
+                        label: [],
+                        backgroundColor: [],
                         borderWidth: 1,
-                        data:[]
+                        fontColor: 'white',
+                        data: []
                     }
                 ],
             },
             phraseData: {
-                labels:[],
+                labels: [],
                 //labels: ['Germany', 'Spain', 'Mexico'],
                 datasets: [
                     {
                         // label: "Sentiment",
                         // data: ['0.2973507046699524','0.8444602489471436','0.9474703073501587']
-                        label:[],
-                        backgroundColor:[],
+                        label: [],
+                        backgroundColor: [],
                         borderWidth: 1,
-                        data:[]
+                        data: []
                     }
                 ],
             }
@@ -73,7 +74,7 @@ const useStyles = makeStyles(theme => ({
                 min: 0
             }
         });
-        var data = this.props.location.state; 
+        var data = this.props.location.state;
         console.log(data);
         // var data = {
         //     "entity": {
@@ -113,13 +114,13 @@ const useStyles = makeStyles(theme => ({
         var colourGen = function () {
             this.at = 0;
             this.colors = [
-                "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
-                "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
-                "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
-                "#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
-                "#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
-                "#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
-                "#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
+                "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177", "#0d5ac1",
+                "#f205e6", "#1c0365", "#14a9ad", "#4ca2f9", "#a4e43f", "#d298e2", "#6119d0",
+                "#d2737d", "#c0a43c", "#f2510e", "#651be6", "#79806e", "#61da5e", "#cd2f00",
+                "#9348af", "#01ac53", "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
+                "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#b0d87b", "#ca4751", "#7e50a8",
+                "#c4d647", "#e0eeb8", "#11dec1", "#289812", "#566ca0", "#ffdbe1", "#2f1179",
+                "#935b6d", "#916988", "#513d98", "#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
                 "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
                 "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
                 "#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf",
@@ -154,9 +155,9 @@ const useStyles = makeStyles(theme => ({
                 "#f812b3", "#b17fc9", "#8d6c2f", "#d3277a", "#2ca1ae", "#9685eb", "#8a96c6",
                 "#dba2e6", "#76fc1b", "#608fa4", "#20f6ba", "#07d7f6", "#dce77a", "#77ecca"]
 
-            this.nextColour = function() {
-                this.at += Math.round(Math.random()*10);
-                return this.colors[this.at%this.colors.length];
+            this.nextColour = function () {
+                this.at += Math.round(Math.random() * 10);
+                return this.colors[this.at % this.colors.length];
             }
         }
 
@@ -178,55 +179,122 @@ const useStyles = makeStyles(theme => ({
                 }
             }
         }
-        console.log(this.state);
+        console.log(this.props.location.state);
 
         return (
             <div>
-                
-            <Print message="Search Results"/>
-            <Grid container spacing={3}>
-                <Grid container justify="center" item xs={6}>
-                
-                    <Bar
-                        data={this.state.phraseData}
-                        width={800}
-                        height={400}
-                        options={{ maintainAspectRatio: true }}
-                        //background = '#fff'
-                    />
-                
-                </Grid>
-                <Grid container justify="center" item xs={6}>
 
-                    <Line
-                        data={this.state.phraseData}
-                        width={800}
-                        height={400}
-                        options={{ maintainAspectRatio: true }}
-                        //background = '#fff'
-                    />
-                
+                <Print message="Search Results" />
+                <Grid container spacing={3}>
+                    <Grid container justify="center" item xs={6}>
+
+                        <Bar
+                            data={this.state.entityData}
+                            width={800}
+                            height={400}
+                            options={
+                                {
+                                    legend: {
+                                        labels: {
+                                            fontColor: 'white'
+                                        },
+                                        yAxes: [{
+                                            ticks: {
+                                                fontColor: 'white'
+                                            },
+                                        }],
+                                        xAxes: [{
+                                            ticks: {
+                                                fontColor: 'white'
+                                            },
+                                        }]
+                                    },
+                                    maintainAspectRatio: true,
+                                    tooltips: {
+                                        callbacks: {
+                                            label: (tooltipItems) => {
+                                                console.log(this.props.location.state.entity);
+                                                var x = this.state.entityData.labels[tooltipItems.index];
+                                                console.log(x);
+                                                return this.props.location.state.entity[x].text;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        />
+
+                    </Grid>
+                    <Grid container justify="center" item xs={6}>
+
+                        <Line
+                            data={this.state.entityData}
+                            width={800}
+                            height={400}
+                            options={
+                                {
+                                    maintainAspectRatio: true,
+                                    tooltips: {
+                                        callbacks: {
+                                            label: (tooltipItems) => {
+                                                console.log(this.props.location.state.entity);
+                                                var x = this.state.entityData.labels[tooltipItems.index];
+                                                console.log(x);
+                                                return this.props.location.state.entity[x].text;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        />
+
+                    </Grid>
+                    <Grid container justify="center" item xs={6}>
+                        <Doughnut
+                            data={this.state.entityData}
+                            width={800}
+                            height={400}
+                            options={
+                                {
+                                    maintainAspectRatio: true,
+                                    tooltips: {
+                                        callbacks: {
+                                            label: (tooltipItems) => {
+                                                console.log(this.props.location.state.entity);
+                                                var x = this.state.entityData.labels[tooltipItems.index];
+                                                console.log(x);
+                                                return this.props.location.state.entity[x].text;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        />
+                    </Grid>
+                    <Grid container justify="center" item xs={6}>
+                        <Radar
+                            data={this.state.entityData}
+                            width={800}
+                            height={400}
+                            options={
+                                {
+                                    maintainAspectRatio: true,
+                                    tooltips: {
+                                        callbacks: {
+                                            label: (tooltipItems) => {
+                                                console.log(this.props.location.state.entity);
+                                                var x = this.state.entityData.labels[tooltipItems.index];
+                                                console.log(x);
+                                                return this.props.location.state.entity[x].text;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        />
+                    </Grid>
+
                 </Grid>
-                <Grid container justify="center" item xs={6}>
-                    <Doughnut
-                        data={this.state.phraseData}
-                        width={800}
-                        height={400}
-                        options={{ maintainAspectRatio: true }}
-                        //background = '#fff'
-                    />
-                </Grid>
-                <Grid container justify="center" item xs={6}>
-                    <Radar
-                        data={this.state.phraseData}
-                        width={800}
-                        height={400}
-                        options={{ maintainAspectRatio: true }}
-                        //background = '#fff'
-                    />
-                </Grid>
-                
-            </Grid>
             </div>
 
         )
